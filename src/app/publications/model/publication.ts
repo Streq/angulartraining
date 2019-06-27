08a095ce570d69842b00be5f1a72abb6668435e5
Requena <Santiago.Requena@thomsonreuters.com>
2019-06-27 15:46:38 -0300

    publicaciones papÁ

---
+++ src/app/publications/model/publication.ts
@@ -0,0 +1,8 @@
+export interface Publication {
+    id: number;
+    title: string;
+    description: string;
+    date: Date;
+    price: number;
+}
+
--- src/app/publications/publications.component.ts
+++ src/app/publications/publications.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Publication } from './model/publication';
 
 @Component({
   selector: 'app-publications',
@@ -6,8 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publications.component.scss']
 })
 export class PublicationsComponent implements OnInit {
+  publications: Publication[] = [
+    {
+      id: 1,
+      title: 'MAF Adventures',
+      description: 'A fantastic journey through MAF',
+      date: new Date('1970-09-25T00:00:00'),
+      price: 0.15
+    },
+    {
+      id: 2,
+      title: 'One Pass for dummies',
+      description: 'Learn how to add a RegKey and much more!',
+      date: new Date('2013-12-25T00:00:00'),
+      price: 5
+    },
+    {
+      id: 3,
+      title: 'Westlaw Asia Roadmap',
+      description: 'The ultimate roadmap with the definitive production dates',
+      date: new Date('2050-01-01T00:00:00'),
+      price: 1000
+    }
+  ];
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
