0c9c5df0782effd6c7e7dfe526a2506aa0ca0009
Requena <Santiago.Requena@thomsonreuters.com>
2019-06-27 16:23:50 -0300

    publications service

--- src/app/publications/publications.component.ts
+++ src/app/publications/publications.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Publication } from './model/publication';
+import { PublicationsService } from './service/publications.service';
 
 @Component({
   selector: 'app-publications',
@@ -7,35 +8,14 @@ import { Publication } from './model/publication';
   styleUrls: ['./publications.component.scss']
 })
 export class PublicationsComponent implements OnInit {
-  publications: Publication[] = [
-    {
-      id: 1,
-      title: 'MAF Adventures',
-      description: 'A fantastic journey through MAF',
-      date: new Date('1970-09-25T00:00:00'),
-      price: 0.15
-    },
-    {
-      id: 2,
-      title: 'One Pass for dummies',
-      description: 'Learn how to add a RegKey and much more!',
-      date: new Date('2013-12-25T00:00:00'),
-      price: 5
-    },
-    {
-      id: 3,
-      title: 'Westlaw Asia Roadmap',
-      description: 'The ultimate roadmap with the definitive production dates',
-      date: new Date('2050-01-01T00:00:00'),
-      price: 1000
-    }
-  ];
+  publications: Publication[];
 
-  constructor() {
+  constructor(private publicationsService: PublicationsService) {
 
   }
 
   ngOnInit() {
+    this.publications = this.publicationsService.getPublications();
   }
 
   share(publication: Publication) {
---
+++ src/app/publications/service/publications.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Publication } from '../model/publication';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PublicationsService {
+  publications: Publication[] = [
+    {
+      id: 1,
+      title: 'MAF Adventures',
+      description: 'A fantastic journey through MAF',
+      date: new Date('1970-09-25T00:00:00'),
+      price: 0.15
+    },
+    {
+      id: 2,
+      title: 'One Pass for dummies',
+      description: 'Learn how to add a RegKey and much more!',
+      date: new Date('2013-12-25T00:00:00'),
+      price: 5
+    },
+    {
+      id: 3,
+      title: 'Westlaw Asia Roadmap',
+      description: 'The ultimate roadmap with the definitive production dates',
+      date: new Date('2050-01-01T00:00:00'),
+      price: 1000
+    }
+  ];
+
+  constructor() { }
+
+  getPublications(): Publication[] {
+    return this.publications;
+  }
+}
